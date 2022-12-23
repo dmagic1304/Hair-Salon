c1d5d477c3a1782b14228edfd30a08975c525cd0
Dominik Magic <dominikmagic5@gmail.com>
2022-12-23 14:23:30 -0800

    WIP: working on displaying different client create views depending if the new client was chosen withing a stylist

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -3,6 +3,7 @@ using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HairSalon.Controllers
 {
@@ -16,11 +17,20 @@ namespace HairSalon.Controllers
     }
 
     public ActionResult Create(int stylistId)
-    {           
+    { 
+      if(stylistId != 0)
+      {          
       Stylist chosenStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == stylistId);
       ViewBag.StylistName = chosenStylist.Name;
       ViewBag.StylistId = stylistId;
       return View();
+      }
+      else
+      {
+        ViewBag.Stylists = _db.Stylists.ToList();
+        ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+        return View();
+      }
     }
 
     [HttpPost]
