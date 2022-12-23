a81165da712c8a477e1bbed96539b0cb71c63f24
Dominik Magic <dominikmagic5@gmail.com>
2022-12-23 08:50:04 -0800

    added Client model

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public string StylistId { get; set; }
+        public Stylist Stylist { get; set; }
+    }
+}
