9e7beacac76f70e97e6346695625136296a9258e
Dominik Magic <dominikmagic5@gmail.com>
2022-12-23 11:14:21 -0800

    BUGFIX: StylistId declared as a wrong tipe, foreign key connection fixed

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -6,7 +6,7 @@ namespace HairSalon.Models
     {
         public int ClientId { get; set; }
         public string Name { get; set; }
-        public string StylistId { get; set; }
+        public int StylistId { get; set; }
         public Stylist Stylist { get; set; }
     }
 }
